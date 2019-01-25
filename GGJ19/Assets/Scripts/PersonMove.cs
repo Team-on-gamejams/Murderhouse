f1c891ef7c4bc289a835a5b5180ab9863baf9b14
Sasha <sdrozdiuk12@gmail.com>
2019-01-26 02:35:08 +0300

    + Click
    :(

--- GGJ19/Assets/Scripts/PersonMove.cs
+++ GGJ19/Assets/Scripts/PersonMove.cs
@@ -2,42 +2,59 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PersonMove : MonoBehaviour {
-	static private GameObject FinishObject;
-	public Transform EndPos;
-	private int RemunerationFroDeath { get; set; }
-	public int ChanceToBecome { get; set; }
-	public readonly int speed = 30;
-	private float SpawnX;
-	private float SpawnY;
-	private float SpawnZ;
-	static private int CheckForOverflow = 0;
-	private readonly int RandomNumForSpawnMoreObjects = 10;
+public class PersonMove : MonoBehaviour
+{
+    static private GameObject FinishObject;
+    public Transform EndPos;
+    private int RemunerationFroDeath { get; set; }
+    public int ChanceToBecome { get; set; }
+    public readonly int speed = 30;
+    private float SpawnX;
+    private float SpawnY;
+    private float SpawnZ;
+    static private int CheckForOverflow = 0;
+    private readonly int RandomNumForSpawnMoreObjects = 7;
+    private readonly int MaxPersonsInAction = 4;
 
-	// Start is called before the first frame update
-	void Start() {
-		FinishObject = GameObject.FindGameObjectWithTag("MyFinishObjct");
-		EndPos = FinishObject.transform;
-		SpawnX = this.transform.position.x;
-		SpawnY = this.transform.position.y;
-		SpawnZ = this.transform.position.z;
+    // Start is called before the first frame update
+    void Start()
+    {
+        FinishObject = GameObject.FindGameObjectWithTag("MyFinishObjct");
+        EndPos = FinishObject.transform;
+        SpawnX = this.transform.position.x;
+        SpawnY = this.transform.position.y;
+        SpawnZ = this.transform.position.z;
 
-	}
+    }
 
-	// Update is called once per frame
-	void Update() {
-		this.transform.position = Vector3.MoveTowards(this.transform.position, EndPos.position, Time.deltaTime * speed);
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = Vector3.MoveTowards(this.transform.position, EndPos.position, Time.deltaTime * speed);
 
-		if (RandomNumForSpawnMoreObjects == Random.Range(0, 100)) {
-			CheckForOverflow++;
-			if (CheckForOverflow < 10) {
-				Instantiate(this.gameObject, new Vector3(SpawnX, SpawnY, SpawnZ), Quaternion.identity);
-			}
-		}
+        if (RandomNumForSpawnMoreObjects == Random.Range(0, 50))
+        {
+            CheckForOverflow++;
 
-		if (this.transform.position == EndPos.position) {
-			Destroy(this.gameObject);
-			CheckForOverflow--;
-		}
-	}
+            if (CheckForOverflow < MaxPersonsInAction)
+            {
+                Instantiate(this.gameObject, new Vector3(SpawnX, SpawnY, SpawnZ), Quaternion.identity);
+            }
+        }
+
+        if (this.transform.position == EndPos.position)
+        {
+            Destroy(this.gameObject);
+            CheckForOverflow--;
+        }
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("++++");
+            //if(//answers zamanili v dom)
+            //{
+                this.gameObject.transform.position = Vector3.MoveTowards(transform.position, GameObject.FindGameObjectWithTag("Hom").transform.position, Time.deltaTime * speed);
+            //}
+        }
+    }
 }
