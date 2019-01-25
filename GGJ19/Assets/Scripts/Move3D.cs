b308f6928b8ac87b15c78c50058fac0b5dd64cbd
Team-on <timonsol13@gmail.com>
2019-01-26 00:38:43 +0200

    Add normal grab system and begin make the floor matrix system

--- GGJ19/Assets/Scripts/Move3D.cs
+++ GGJ19/Assets/Scripts/Move3D.cs
@@ -2,44 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Move3D : MonoBehaviour {
+public class Move3D : MonoBehaviour 
+{
 
 	public float distance;
-	public float moveSpeed;
 
 	public Camera _camera;
+
+	private GameObject gm;
+	public float speed = 1f;
+	public bool pressed;
 
 	void Start() {
 
 	}
 
-	void Update() {
+	void FixedUpdate() {
 		Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
 		Debug.DrawRay(ray.origin, ray.direction * 10, Color.yellow);
 
-		Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance); //переменной записываються координаты мыши по иксу и игрику
-		Vector3 objPosition = _camera.ScreenToWorldPoint(mousePosition); // переменной - объекту присваиваеться переменная с координатами мыши
-
-		transform.position = objPosition * moveSpeed;
-
+		
+		if (Physics.Raycast(ray, out hit)) {			
+			if (Input.GetMouseButtonDown(0))
+			{
+				if (hit.collider.CompareTag("furniture")) {
+					gm = hit.collider.gameObject;
+					pressed = !pressed;
+					if (!pressed)
+						gm = null;
+				}
+			}
+		}
+		
+		if (gm != null)
+		{
+			Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
+			Vector3 objPosition = _camera.ScreenToWorldPoint(mousePosition);
+			gm.transform.position = objPosition * speed;
+		}
 
 	}
-
-
-	//void Update()
-	//{
-
-	//	//Vector3 p =	_camera.ScreenToWorldPoint(Input.mousePosition);
-
-	//	//Vector3 wp = _camera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, _camera.nearClipPlane));
-	//	//transform.position = wp - transform.position;
-
-
-	//	//float h = horizontalSpeed * Input.GetAxis("Mouse X") * Time.deltaTime;
-	//	//transform.position = new Vector3(h, 0f, 0f);
-
-	//	//Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance); // переменной записываються координаты мыши по иксу и игрику
-	//	//Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition); // переменной - объекту присваиваеться переменная с координатами мыши
-	//	//transform.position = objPosition; // и собственно объекту записываються координаты
-	//}
-}
+	}
