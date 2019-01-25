d037a117a8e0db89c588720b5673e9e33d2cbb68
Team-on <timonsol13@gmail.com>
2019-01-25 20:24:57 +0200

    Move objects with mouse

---
+++ GGJ19/Assets/Scripts/Move3D.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move3D : MonoBehaviour {
+
+	public float distance;
+	public float moveSpeed;
+
+	public Camera _camera;
+
+	void Start() {
+
+	}
+
+	void Update() {
+		Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+		Debug.DrawRay(ray.origin, ray.direction * 10, Color.yellow);
+
+		Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance); //переменной записываються координаты мыши по иксу и игрику
+		Vector3 objPosition = _camera.ScreenToWorldPoint(mousePosition); // переменной - объекту присваиваеться переменная с координатами мыши
+
+		transform.position = objPosition * moveSpeed;
+
+
+	}
+
+
+	//void Update()
+	//{
+
+	//	//Vector3 p =	_camera.ScreenToWorldPoint(Input.mousePosition);
+
+	//	//Vector3 wp = _camera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, _camera.nearClipPlane));
+	//	//transform.position = wp - transform.position;
+
+
+	//	//float h = horizontalSpeed * Input.GetAxis("Mouse X") * Time.deltaTime;
+	//	//transform.position = new Vector3(h, 0f, 0f);
+
+	//	//Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance); // переменной записываються координаты мыши по иксу и игрику
+	//	//Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition); // переменной - объекту присваиваеться переменная с координатами мыши
+	//	//transform.position = objPosition; // и собственно объекту записываються координаты
+	//}
+}
