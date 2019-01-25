a92e2ee996f4d9ddfefc3122fbbd0a9912251ee2
Team <timonsol13@gmail.com>
2019-01-26 01:03:24 +0200

    Fix spawn bug

--- GGJ19/Assets/Scripts/PersonMove.cs
+++ GGJ19/Assets/Scripts/PersonMove.cs
@@ -2,47 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PersonMove : MonoBehaviour
-{
-    static private GameObject FinishObject;
-    public Transform EndPos;
-    private int RemunerationFroDeath { get; set; }
-    public int ChanceToBecome { get; set; }
-    public readonly int speed = 30;
-    private float SpawnX;
-    private float SpawnY;
-    private float SpawnZ; 
-    private int CheckForOverflow = 0;
-    private readonly int RandomNumForSpawnMoreObjects = 10;
+public class PersonMove : MonoBehaviour {
+	static private GameObject FinishObject;
+	public Transform EndPos;
+	private int RemunerationFroDeath { get; set; }
+	public int ChanceToBecome { get; set; }
+	public readonly int speed = 30;
+	private float SpawnX;
+	private float SpawnY;
+	private float SpawnZ;
+	static private int CheckForOverflow = 0;
+	private readonly int RandomNumForSpawnMoreObjects = 10;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        FinishObject = GameObject.FindGameObjectWithTag("MyFinishObjct");
-        EndPos = FinishObject.transform;
-        SpawnX = this.transform.position.x;
-        SpawnY = this.transform.position.y;
-        SpawnZ = this.transform.position.z;
+	// Start is called before the first frame update
+	void Start() {
+		FinishObject = GameObject.FindGameObjectWithTag("MyFinishObjct");
+		EndPos = FinishObject.transform;
+		SpawnX = this.transform.position.x;
+		SpawnY = this.transform.position.y;
+		SpawnZ = this.transform.position.z;
 
-    }
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        this.transform.position = Vector3.MoveTowards(this.transform.position, EndPos.position, Time.deltaTime * speed);
-        
-        if (RandomNumForSpawnMoreObjects == Random.Range(0, 100))
-        {
-            CheckForOverflow++;
-            if (CheckForOverflow < 10)
-            {
-                Instantiate(this.gameObject, new Vector3(SpawnX, SpawnY, SpawnZ), Quaternion.identity);
-            }
-        }
-        if (this.transform.position == EndPos.position)
-        {
-            Destroy(this.gameObject);
-            CheckForOverflow--;
-        }
-    }
+	// Update is called once per frame
+	void Update() {
+		this.transform.position = Vector3.MoveTowards(this.transform.position, EndPos.position, Time.deltaTime * speed);
+
+		if (RandomNumForSpawnMoreObjects == Random.Range(0, 100)) {
+			CheckForOverflow++;
+			if (CheckForOverflow < 10) {
+				Instantiate(this.gameObject, new Vector3(SpawnX, SpawnY, SpawnZ), Quaternion.identity);
+			}
+		}
+
+		if (this.transform.position == EndPos.position) {
+			Destroy(this.gameObject);
+			CheckForOverflow--;
+		}
+	}
 }
