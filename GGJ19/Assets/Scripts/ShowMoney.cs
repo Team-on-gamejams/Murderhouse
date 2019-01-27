31426dff2b4d05775c2c005ba7998fd56e613c28
Bandera1 <pearmadn@gmail.com>
2019-01-27 13:38:57 +0200

    Add money indicator

---
+++ GGJ19/Assets/Scripts/ShowMoney.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowMoney : MonoBehaviour
+{
+	public Text myText;
+	public MoneyController _money;
+
+	private void Update() {
+		myText.text = _money.money.ToString();
+	}
+}
