3402cd1b25d30affaf5220dbad140f3b5685e51e
Bandera1 <pearmadn@gmail.com>
2019-01-27 10:57:50 +0200

    Add TransparentWallScript and HomeController

---
+++ GGJ19/Assets/Scripts/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HomeController : MonoBehaviour
+{
+	[SerializeField] private int money;
+	
+	public bool Minus(int _cost)
+	{
+		if (money >= _cost)
+		{
+			money -= _cost;
+			return true;
+		} else {
+			Debug.Log("Enough money!");
+			return false;
+		}
+	}
+
+	public int Money()
+	{
+		return money;
+	}
+}
---
+++ GGJ19/Assets/Scripts/TransparentWallsScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransparentWallsScript : MonoBehaviour
+{
+	[SerializeField] private float minAlpha;
+
+	[SerializeField] private GameObject[] WallsArr;
+
+	Renderer _rend;
+
+	public void Activate()
+	{
+		foreach (GameObject i in WallsArr) //the line the error is pointing to
+		{
+			_rend = i.GetComponent<Renderer>();
+			if (_rend != null) 
+			{
+
+				i.layer = 2;
+				Color oldColor = _rend.material.color;
+				Color newColor = new Color(oldColor.r, oldColor.g, oldColor.b, minAlpha);
+				_rend.material.SetColor("_Color", newColor);
+			}
+			_rend = null;
+
+		}
+	}
+
+	public void Deactivate()
+	{
+		foreach (GameObject i in WallsArr) //the line the error is pointing to
+			{
+			_rend = i.GetComponent<Renderer>();
+			if (_rend != null) {
+				i.layer = 0;
+				Color oldColor = _rend.material.color;
+				Color newColor = new Color(oldColor.r, oldColor.g, oldColor.b,1f);
+				_rend.material.SetColor("_Color", newColor);
+			}
+			_rend = null;
+
+		}
+	}
+}
